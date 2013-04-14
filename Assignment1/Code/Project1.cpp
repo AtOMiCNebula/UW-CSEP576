ba0190ae2e606160ba703ddfce8295142a47b256
Jeff <Nebula@Planet-Nebula.com>
2013-04-13 20:23:02 -0700

    CSEP576-1: SeparableGaussianBlurImage implemented
    Also refactored GaussianBlurImage to allow breaking out the convolution
    code, so that it could be re-used for SeparableGaussianBlurImage.

--- Assignment1/Code/Project1.cpp
+++ Assignment1/Code/Project1.cpp
@@ -173,6 +173,48 @@ void MainWindow::HalfImage(QImage &image)
 
 
 
+void ConvolveHelper(QImage *image, double *kernel, int kernelWidth, int kernelHeight)
+{
+    int height = image->height();
+    int width = image->width();
+    int kernelHalfHeight = (kernelHeight / 2);
+    int kernelHalfWidth = (kernelWidth / 2);
+
+    QImage buffer = image->copy(-kernelHalfWidth, -kernelHalfHeight, width + 2*kernelHalfWidth, height + 2*kernelHalfHeight );
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            double rgb[3];
+            rgb[0] = 0.0;
+            rgb[1] = 0.0;
+            rgb[2] = 0.0;
+
+            for (int fy = 0; fy < kernelHeight; fy++)
+            {
+                for (int fx = 0; fx < kernelWidth; fx++)
+                {
+                    // Translate to coordinates in buffer space
+                    int by = kernelHalfHeight + y + (fy-kernelHalfHeight);
+                    int bx = kernelHalfWidth + x + (fx-kernelHalfWidth);
+
+                    double kernelWeight = kernel[fy*kernelWidth+fx];
+                    QRgb pixel = buffer.pixel(bx, by);
+                    rgb[0] += kernelWeight*qRed(pixel);
+                    rgb[1] += kernelWeight*qGreen(pixel);
+                    rgb[2] += kernelWeight*qBlue(pixel);
+                }
+            }
+
+            image->setPixel(x, y, qRgb(
+                        static_cast<int>(floor(rgb[0]+0.5)),
+                        static_cast<int>(floor(rgb[1]+0.5)),
+                        static_cast<int>(floor(rgb[2]+0.5))
+                    ));
+        }
+    }
+}
+
 void MainWindow::GaussianBlurImage(QImage *image, double sigma)
 {
     if (sigma <= 0)
@@ -196,42 +238,7 @@ void MainWindow::GaussianBlurImage(QImage *image, double sigma)
     }
 
     // Generate the updated image
-    int height = image->height();
-    int width = image->width();
-    QImage buffer = image->copy(-kernelHalfSide, -kernelHalfSide, width + 2*kernelHalfSide, height + 2*kernelHalfSide );
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            double rgb[3];
-            rgb[0] = 0.0;
-            rgb[1] = 0.0;
-            rgb[2] = 0.0;
-
-            for (int fy = 0; fy < kernelSize; fy++)
-            {
-                for (int fx = 0; fx < kernelSize; fx++)
-                {
-                    // Translate to coordinates in buffer space
-                    // (kernelHalfSide-length borders on each side)
-                    int bx = kernelHalfSide + x + (fx-kernelHalfSide);
-                    int by = kernelHalfSide + y + (fy-kernelHalfSide);
-
-                    double kernelWeight = kernel[fy*kernelSize+fx];
-                    QRgb pixel = buffer.pixel(bx, by);
-                    rgb[0] += kernelWeight*qRed(pixel);
-                    rgb[1] += kernelWeight*qGreen(pixel);
-                    rgb[2] += kernelWeight*qBlue(pixel);
-                }
-            }
-
-            image->setPixel(x, y, qRgb(
-                        static_cast<int>(floor(rgb[0]+0.5)),
-                        static_cast<int>(floor(rgb[1]+0.5)),
-                        static_cast<int>(floor(rgb[2]+0.5))
-                    ));
-        }
-    }
+    ConvolveHelper(image, kernel, kernelSize, kernelSize);
 
     // Clean up!
     delete[] kernel;
@@ -239,7 +246,29 @@ void MainWindow::GaussianBlurImage(QImage *image, double sigma)
 
 void MainWindow::SeparableGaussianBlurImage(QImage *image, double sigma)
 {
-    // Add your code here.  Done right, you should be able to copy most of the code from GaussianBlurImage.
+    if (sigma <= 0)
+    {
+        return;
+    }
+
+    // Calculate the kernel (some extra computation/storage, should be fine...)
+    double twoSigSq = 2.0 * pow(sigma, 2);
+    double sigSqRt = sigma * sqrt(2*M_PI);
+    int kernelHalfSide = static_cast<int>(ceil(3 * sigma));
+    int kernelSize = ((2 * kernelHalfSide) + 1);
+    double *kernel = new double[kernelSize];
+    for (int i = 0; i < kernelSize; i++)
+    {
+        int y = (i - kernelHalfSide);
+        kernel[i] = (1.0 / (sigSqRt)) * pow(M_E, -1*(pow(y,2.0))/twoSigSq);
+    }
+
+    // Generate the updated image
+    ConvolveHelper(image, kernel, kernelSize, 1);
+    ConvolveHelper(image, kernel, 1, kernelSize);
+
+    // Clean up!
+    delete[] kernel;
 }
 
 void MainWindow::FirstDerivImage(QImage *image, double sigma)
